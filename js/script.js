6ebac9ed7376662bb8adf8dc934c3b8eb1b6b02d
Forklein <giuseppepisani2@gmail.com>
2021-07-28 14:39:28 +0200

    Print all image

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ console.log('Vue is activated', Vue);
 const app = new Vue({
     el: '#slider',
     data: {
-        test: 'Hi vue!'
+        test: 'Hi vue!',
+        images: [
+            "images/image1.jpg",
+            "images/image2.jpg",
+            "images/image3.jpg",
+            "images/image4.jpg",
+        ]
     },
     methods: {
         bye() {
